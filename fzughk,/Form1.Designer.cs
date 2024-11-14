2dc56b68774c38654eda228d875c8e4f7a1ae9ad
Kunics.Attila <Kunics.Attila@C19-11>
2024-11-14 20:58:38 +0100

    3.feladat kész.

--- fzughk,/Form1.Designer.cs
+++ fzughk,/Form1.Designer.cs
@@ -29,13 +29,119 @@ namespace fzughk_
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.fatorzs = new System.Windows.Forms.PictureBox();
+            this.falomb = new System.Windows.Forms.PictureBox();
+            this.embertest = new System.Windows.Forms.PictureBox();
+            this.emberfej = new System.Windows.Forms.PictureBox();
+            this.emberkar = new System.Windows.Forms.PictureBox();
+            this.kosar = new System.Windows.Forms.PictureBox();
+            this.kosarful = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.fatorzs)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.falomb)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.embertest)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emberfej)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emberkar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kosar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kosarful)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fatorzs
+            // 
+            this.fatorzs.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.fatorzs.Location = new System.Drawing.Point(118, 195);
+            this.fatorzs.Name = "fatorzs";
+            this.fatorzs.Size = new System.Drawing.Size(101, 243);
+            this.fatorzs.TabIndex = 0;
+            this.fatorzs.TabStop = false;
+            // 
+            // falomb
+            // 
+            this.falomb.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.falomb.Location = new System.Drawing.Point(61, 70);
+            this.falomb.Name = "falomb";
+            this.falomb.Size = new System.Drawing.Size(228, 134);
+            this.falomb.TabIndex = 1;
+            this.falomb.TabStop = false;
+            // 
+            // embertest
+            // 
+            this.embertest.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.embertest.Location = new System.Drawing.Point(560, 320);
+            this.embertest.Name = "embertest";
+            this.embertest.Size = new System.Drawing.Size(52, 118);
+            this.embertest.TabIndex = 2;
+            this.embertest.TabStop = false;
+            // 
+            // emberfej
+            // 
+            this.emberfej.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.emberfej.Location = new System.Drawing.Point(532, 262);
+            this.emberfej.Name = "emberfej";
+            this.emberfej.Size = new System.Drawing.Size(92, 61);
+            this.emberfej.TabIndex = 3;
+            this.emberfej.TabStop = false;
+            // 
+            // emberkar
+            // 
+            this.emberkar.BackColor = System.Drawing.Color.Yellow;
+            this.emberkar.Location = new System.Drawing.Point(504, 357);
+            this.emberkar.Name = "emberkar";
+            this.emberkar.Size = new System.Drawing.Size(74, 36);
+            this.emberkar.TabIndex = 4;
+            this.emberkar.TabStop = false;
+            // 
+            // kosar
+            // 
+            this.kosar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.kosar.Location = new System.Drawing.Point(618, 357);
+            this.kosar.Name = "kosar";
+            this.kosar.Size = new System.Drawing.Size(111, 80);
+            this.kosar.TabIndex = 5;
+            this.kosar.TabStop = false;
+            // 
+            // kosarful
+            // 
+            this.kosarful.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.kosarful.Location = new System.Drawing.Point(665, 310);
+            this.kosarful.Name = "kosarful";
+            this.kosarful.Size = new System.Drawing.Size(18, 50);
+            this.kosarful.TabIndex = 6;
+            this.kosarful.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.kosarful);
+            this.Controls.Add(this.kosar);
+            this.Controls.Add(this.emberkar);
+            this.Controls.Add(this.emberfej);
+            this.Controls.Add(this.embertest);
+            this.Controls.Add(this.falomb);
+            this.Controls.Add(this.fatorzs);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.fatorzs)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.falomb)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.embertest)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emberfej)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emberkar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kosar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kosarful)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox fatorzs;
+        private System.Windows.Forms.PictureBox falomb;
+        private System.Windows.Forms.PictureBox embertest;
+        private System.Windows.Forms.PictureBox emberfej;
+        private System.Windows.Forms.PictureBox emberkar;
+        private System.Windows.Forms.PictureBox kosar;
+        private System.Windows.Forms.PictureBox kosarful;
     }
 }
 
--- fzughk,/Form1.cs
+++ fzughk,/Form1.cs
@@ -8,13 +8,97 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+/*Hozz létre egy fát és egy embert, az embernek nem kötlező hogy feje legyen  fatorzs, falomb, embertest, emberkar, emberfej
+Hozz létre egy kosarat
+Az ember a kosár és a fa törzse között sétál oda-vissza
+Amikor a fához ér, elkezdi azt a kezével ütni. Az ütés animálása kötelező
+Miután az ember 10-szer megütötte a fát, egy alma hulljon le a lombból, pont az ember kezébe. A kezénél álljon meg
+Amikor megvolt a 10. ütés, nem kell többet ütni, már csak a hulló almára vár az ember
+Amint elkapta az almát, visszafordul, ez lehet egy hirtelen fordulás, majd elindul almával a kezében vissza a kosárhoz
+Amikor elérte a kosarat, a kezében lévő alma folytatja lefelé hullását addig, amíg a kosár teljesen el nem nyeli
+Amikor a kosár elnyelte az almát, a gyűjtött almák száma eggyel nő
+  Ha a gyűjtött almák száma elérte a kosár teherbírását, több almát nem tárolunk el
+Amint leesett az alma és elnyelte a kosár, az ember visszafordul és újrakezdi útját
+
+A nagyobb kosár vásárlása gomb illetve a gyorsabb szedés gomb csak akkor működik, ha van elég almánk a fejlesztés megvásárlásához.
+   nagyobb kosár vásárlása esetén:
+     a kosár teherbírása 5 almával nő
+     a nagyobb kosár ára 2 almával nő
+   gyorsabb szedés vásárlása esetén:
+    az alma leszedéséhez 1-el kevesebb ütés szükséges, de minimum 3
+    a gyorsabb szedés ára 30 almával nő*/
+
+
 namespace fzughk_
 {
     public partial class Form1 : Form
     {
+        int[] positions = new int[2];
+        Random r = new Random();
+        Timer MozgasTimer = new Timer();
+        Timer UtesTimer = new Timer();
+
+        //Label alma = new Label();
+        //this.Controls.Add(alma);
+        //this.Controls.Add(ember);
+
         public Form1()
         {
             InitializeComponent();
+            Start();
+
+        }
+        void Start()
+        {
+            List<PictureBox> ember = new List<PictureBox>();
+            //this.Controls.Add(ember);
+            //positions[0] = this.ClientSize.Width - ember.Left
+            Label alma = new Label();
+            this.Controls.Add(alma);
+            alma.Width = 30;
+            alma.Height = 30;
+            alma.Left = this.ClientSize.Width / 4 + alma.Width;
+            alma.Top = this.falomb.Bottom - alma.Height / 2;
+            alma.BackColor = Color.Red;
+            emberMozgas();
+            faUtes();
+        }
+
+        void emberMozgas()
+        {
+            List<PictureBox> ember = new List<PictureBox>();
+            ember.Add(embertest);
+            ember.Add(emberkar);
+            ember.Add(emberfej);
+
+            Timer MozgasTimer = new Timer();
+            MozgasTimer.Interval = 1000;
+            MozgasTimer.Start();
+            MozgasTimer.Tick += (s, e) =>
+            {
+                foreach (PictureBox item in ember)
+                {
+                    if (item.Visible)
+                    {
+                        item.Left = fatorzs.Left + fatorzs.Width - emberkar.Width;
+                        //item.Left = positons[1];
+                    }
+                    else if (item.Visible)
+                    {
+                        positions[0] = kosar.Left - emberkar.Width;
+                        item.Left = positions[0];
+                    };
+                };
+            };
+        }
+        void faUtes()
+        {
+            //Timer UtesTimer = new Timer();
+            UtesTimer.Start();
+            UtesTimer.Tick += (ss, ee) =>
+            {
+                UtesTimer.Interval = 10;
+            };
         }
     }
 }
